d087c0316889cf880af1a4eb4cb097e1392bd3bc
bjd2385 <brandon.doyle@hqo.co>
2022-05-13 16:53:58 -0400

    Add command back

--- src/validate.sh
+++ src/validate.sh
@@ -13,3 +13,5 @@ PREPACK="${2:-false}"
 if [ "$PREPACK" = true ]; then
     find "$SRC" -maxdepth 1 -mindepth 1 -type d -print0 | xargs --null -I % basename % | xargs --null -I % ./scripts/pre-pack.sh "$SRC" %
 fi
+
+circleci orb validate <(circleci orb pack "$SRC") >/dev/null
